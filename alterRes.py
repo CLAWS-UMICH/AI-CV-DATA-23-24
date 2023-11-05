5539acf523e2872beb548a661184ee33feaa7a26
rchandel <rchandel@umich.edu>
2023-11-05 18:11:39 -0500

    added additional parameter for prefix and fixed image rotating issue

--- alterRes.py
+++ alterRes.py
@@ -1,11 +1,12 @@
 import os
 import argparse
-from PIL import Image
+from PIL import Image, ImageOps
 
 parser = argparse.ArgumentParser(description = "changes resolution of images")
 
 parser.add_argument('input_folder', help = 'Path to image that needs to be alterred')
 parser.add_argument('output_folder', help = 'Path to image that needs to be alterred')
+parser.add_argument('prefix', help = 'prefix for files')
 parser.add_argument('new_width', type=int, help = 'width of new image')
 parser.add_argument('new_height', type=int, help = 'height of new image')
 parser.add_argument('--keep_ratio', '-k', action='store_true', help = 'keeps ratio and chooses closest value to values defined earlier')
@@ -20,12 +21,16 @@ for filename in os.listdir(args.input_folder):
     file_path = os.path.join(args.input_folder, filename)
 
     image = Image.open(file_path)
+    image = ImageOps.exif_transpose(image)
     width, height = image.size
 
     if args.keep_ratio:
         q_width = width / args.new_width
         q_height = height / args.new_height
-        scale = q_width if q_width > q_height else q_height
+        if q_width > q_height:
+            scale = q_width
+        else:
+            scale = q_height
         final_width = int(width//scale)
         final_height = int(height//scale)
         resized_image = image.resize((final_width, final_height), Image.LANCZOS)
@@ -36,6 +41,6 @@ for filename in os.listdir(args.input_folder):
 
     if args.verbose:
         print(f"New width: {final_width}, New height: {final_height}")
-    output_filename = f"data_{filename}"
+    output_filename = f"{args.prefix}_{filename}"
     output_path = os.path.join(args.output_folder, output_filename)
     resized_image.save(output_path)
